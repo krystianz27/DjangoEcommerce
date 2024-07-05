f89e688427d82d3503c16a3a83e1dcfec0e8b1ab
Krystian <226311@student.uek.krakow.pl>
2024-07-05 15:00:45 +0200

    Account password update

--- account/forms.py
+++ account/forms.py
@@ -45,7 +45,7 @@ class UpdateUserForm(forms.ModelForm):
 
     class Meta:
         model = User
-        fields = ["username", "email", "password"]
+        fields = ["username", "email"]
 
     def __init__(self, *args, **kwargs):
         super(UpdateUserForm, self).__init__(*args, **kwargs)
